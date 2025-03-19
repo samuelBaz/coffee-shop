8c689cd0594c14ed8ad0e4f036683067bcdea636
Bazoalto Samuel <bazoaltosamuel33@outlook.com>
2025-03-18 22:06:07 -0400

    feat: sub-class

--- src/main/java/com/coffee/shop/Main.java
+++ src/main/java/com/coffee/shop/Main.java
@@ -3,7 +3,7 @@ package com.coffee.shop;
 public class Main {
     public static void main(String[] args) {
         CoffeeShop coffeeShop = new CoffeeShop();
-        Coffee myCoffee = new Coffee();
+        CoffeeWithMilk myCoffee = new CoffeeWithMilk();
         coffeeShop.printInvoice(myCoffee);
     }
 }
